3bfb43bfadc879bea8b8e30ef870e9f849c50fc2
Hai <wordpree.dev@gmail.com>
2018-02-02 14:47:08 +1000

    updated gulp.js with new plugins:gulp-notify,gulp-sass,gulp-clean-css,gulp-jshint,del,autoprefixer

--- wp-content/themes/workflow/gulpfile.js
+++ wp-content/themes/workflow/gulpfile.js
@@ -1,72 +1,113 @@
+
 /*
-* project configuration :setting parths requirements 
+* project configuration :setting parth requirements 
 */
-var name       = 'storefront-child';
+var name        = 'storefront-child';
 var projectRoot = '../' + name + '/';
-var styleSrc    = projectRoot + 'sass/';
-var styleDes    = projectRoot;
-var styleMode,flag;
 
- flag = process.env.NODE_ENV
- if (flag =='development'){
-    styleMode = 'expanded';
- }else{
-    styleMode = 'compressed';
- }
+var path ={
+	src      : projectRoot + 'assets/**/source/',
+	styleSrc : projectRoot + 'assets/sass/source/',
+    jsSrc    : projectRoot + 'assets/js/source/',
 
+    dev      : projectRoot + 'assets/**/development/',
+	styleDev : projectRoot + 'assets/sass/development/',
+    jsDev    : projectRoot + 'assets/js/development/',
+
+    dist      : projectRoot + 'assets/**/production/',
+	styleDist : projectRoot + 'assets/sass/production/',
+    jsDist    : projectRoot + 'assets/js/production/'.
+
+    css  : projectRoot + 'assets/sass/',
+    js   : projectRoot + 'assets/js/'
+};
+
+var DEV_DIST  = process.env.NODE_ENV;
+var dir = DEV_DIST ? 'production' : 'development';
 /*
 * loading plugins
 */
-var gulp       = require('gulp'),
-    gUtil      = require('gulp-util'),
-    gIf        = require('gulp-if'),
-    Sourcempas =require('gulp-sourcemaps'),
-    gulpSass   = require('gulp-compass'),
-    gulpJslint   = require('gulp-jslint'),
-    gulpUglify = require('gulp-uglify'),
-    gulpCat    = require('gulp-concat'),
-    gulpRename = require('gulp-rename'),
-    livereload = require('gulp-livereload');
+var gulp         = require('gulp'),
+    gulpIf       = require('gulp-if'),
+    gUtil        = require('gulp-util'),
+    gulpNotify   = require('gulp-notify'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    gulpSass     = require('gulp-sass'),
+    gulpCleanCss = require('gulp-clean-css'),
+    gulpJshint   = require('gulp-jshint'),
+    gulpUglify   = require('gulp-uglify'),
+    gulpCat      = require('gulp-concat'),
+    gulpPrefix   = require('autoprefixer'),
+    gulpPcss     = require('gulp-postcss'),
+    gulpDel      = require('del'),
+    gulpRename   = require('gulp-rename');
     
 /*
-* gulp tasks
+* gulp tasks: 
 */
 
-gulp.task('sass',function(){
-	return gulp.src(styleSrc + 'style.scss')
+/*source pipe into development/debug version*/
+gulp.task('sass:dev',function(){
+	return gulp.src(path.styleSrc + 'style.scss')
+	       .pipe(sourcemaps.init())
 	       .pipe(gulpSass({
-	       	 sass: styleSrc,
-	       	 css:  styleDes,
-	       	 style: styleMode,
-	       	 sourcemap: 'true'
-	       }))
-	       .pipe(gIf(flag == 'production',gulpRename({suffix:'min'})))
-	       .pipe(gulp.dest(styleDes))
-	       .pipe(livereload());
-	       
+	       	outputStyle: 'expanded'
+	       }).on('error',gulpSass.logError))
+	       .pipe(gulpPcss([gulpPrefix('last 2 versions','> 2%')]))
+	       .pipe(sourcemaps.write('./maps'))
+	       .pipe(gulp.dest(path.styleDev));
 });
 
-gulp.task('jsLint',function(){
-	return gulp.src(styleSrc +'source/js/**.js')
-	    .pipe(gulpJslint())
-	    .pipe(gulpJslint.reporter('default'));
+gulp.task('jshint:dev',function(){
+	return gulp.src(path.jsSrc +'**.js')
+	    .pipe(gulpJshint())
+	    .pipe(gulpJshint.reporter('jshint-stylish',{ verbose: true }))
+	    .pipe(gulpJshint.reporter('fail'));
 });
 
-gulp.task('js',function(){
-	return gulp.src('source/js/**.js')
-	   .pipe(Sourcempas.init())
-	   .pipe(gulpCat('main.js',{newline:'\r\n'}))
-	   .pipe(gIf(flag == 'production',gulpUglify()))
-	   .pipe(gIf(flag == 'production',gulpRename({suffix:'.min'})))
-	   .pipe(Sourcempas.write())
-	   .pipe(gulp.dest('build/' +dir+ '/js'))
-	   .pipe(livereload());
+gulp.task('js:dev',function(){
+	return gulp.src(path.jsSrc +'**.js')
+	   .pipe(gulp.dest(path.jsDev));
 });
 
-gulp.task('watch',function(){
-	 livereload.listen();
-	 gulp.watch([styleSrc + '**.scss'],['sass']);
-	 gulp.watch(['source/js/**.js'],['jsLint','js']);
-});
-gulp.task('default',['jsLint','js','sass','watch']);
+gulp.task('dev',['sass:dev','jshint:dev','js:dev']);
 
+/*dev pipe into production/release version*/
+gulp.task('css:dist',function(){
+	return gulp.src(path.styleDev + '**.css')
+	    .pipe(gulpCleanCss())
+	    .pipe(gulpRename({suffix:'.min'}))
+	    .pipe(gulp.dest(path.styleDist));
+});
+
+gulp.task('js:dist',function(){
+	return gulp.src(path.jsDev + '**.js')
+	    .pipe(gulpUglify())
+	    .pipe(gulpRename({suffix:'.min'}))
+	    .pipe(gulp.dest(path.jsDist));
+});
+
+gulp.task('dist',['css:dist','js:dist,']);
+
+/*copy assets to reference directory depending on either development or production environment*/
+gulp.task('build:css','css:dist',function(){
+	return gulp.src(path.css + dir + '/**.css')
+	    .pipe(gulp.dest(path.css));
+});
+
+gulp.task('build:js','js:dist',function(){
+	return gulp.src(path.js + dir + '/**.js')
+	    .pipe(gulp.dest(path.js));
+});
+
+
+gulp.task('build',['build:css','build:js']);
+
+/*delete unwanted folders*/
+gulp.task('delete',function(){
+	return del.sync([src,dev,dist]);
+});
+
+
+
+gulp.task('default',['dev','dist']);
