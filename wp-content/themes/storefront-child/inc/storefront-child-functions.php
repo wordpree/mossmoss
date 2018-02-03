4822c2629468abefa55e36d27f435392089d7b9f
Hai <wordpree.dev@gmail.com>
2018-02-03 15:28:12 +1000

    added comments for storefront-child-functions.php and modified style enqueue path

--- wp-content/themes/storefront-child/inc/storefront-child-functions.php
+++ wp-content/themes/storefront-child/inc/storefront-child-functions.php
@@ -1,6 +1,18 @@
 <?php
-/*create by Hai*/
-/*for the nav area*/
+/*
+** create time  : 04-2017
+** last modify  : 2-2018
+** author: create by Hai
+** description:register
+** notice :generally ,enqueue files are under assets folder located in theme directory
+** modification record && time:
+**                      1.modified style enqueue path to new structure && 03-02-2018
+**                      2.
+**                      3.
+**                      4.
+**                      5.
+*/
+/*nav menu register*/
 function register_yee_menus(){
 	register_nav_menus(
 	  array(
@@ -12,14 +24,14 @@ function register_yee_menus(){
 }
 add_action('init','register_yee_menus');
 
-
-/*register child style*/
+/*enqueue child style*/
 function storefront_child_scripts_enqueue(){
 	wp_enqueue_style('storefront-style',get_template_directory_uri().'/style.css');
-	wp_enqueue_style('storefront-child-style',get_stylesheet_directory_uri().'/style.css',array('storefront-style'),'v8.04');
+	wp_enqueue_style('storefront-child-style',get_stylesheet_directory_uri().'/style.css',array('storefront-style'),'v4.17');
+	wp_enqueue_style('storefront-child-main-style',get_stylesheet_directory_uri().'/assets/sass/style.css',array(''),'v2.18');
 	wp_enqueue_style('fancyslider-style',get_stylesheet_directory_uri().'/assets/fancyslider.css',array(),'v9.19');
 	
-	wp_enqueue_script('storefront-child-jquery',get_stylesheet_directory_uri().'/dist/js/vendor/jquery-3.2.1.min.js');
+	wp_enqueue_script('storefront-child-jquery',get_stylesheet_directory_uri().'/assets/js/vendor/jquery-3.2.1.min.js');
 	wp_enqueue_script('fancySlider',
 					  get_stylesheet_directory_uri().'/assets/js/jquery.fancyslider.js',array('jquery'),'v0907',false);
 	
