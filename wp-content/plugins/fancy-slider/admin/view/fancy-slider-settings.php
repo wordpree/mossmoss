2235521ae5210ba47b10f38dd8f3d390708b1d3f
Hai <wordpree.dev@gmail.com>
2018-05-13 22:34:52 +1000

    added fn fancy_slider_text_field fancy_slider_select_field fancy_slider_field_callback_center, deleted fancy_slider_field_callback_mod

--- wp-content/plugins/fancy-slider/admin/view/fancy-slider-settings.php
+++ wp-content/plugins/fancy-slider/admin/view/fancy-slider-settings.php
@@ -51,190 +51,221 @@ function fancy_slider_section_callback_advanced(){
 }
 
 /**
-* function to creat general select field *
+* function to creat text section*
 *@since 0.1.0
 *@var function
 *@return void
-*@param $key -- the key of fancy_slider_options array
-*       $elements -- the available values stored in array to a specific key above
-*       $name  --  the part name of options displayed in fields
+*@param $key -- string, $key in options
+*       $elements -- array, a set of values in options
 **/
-function fancy_slider_select_field_callback($key,$elements,$name){
+function fancy_slider_text_field($key){
     $options = get_option( 'fancy_slider_options' );
-    $mode_type = empty( $options[$key] ) ? '': esc_attr( $options[$key] );
-    echo "<select name='fancy_slider_options[$key]'>";
+    $name = "fancy_slider_options[$key]";
+    if ($key === 'slider_ctr') {
+        $options= &$options['slider_ctr'];
+        $key = 'pad';
+        $name = "fancy_slider_options[slider_ctr][pad]";
+    }
+    $value = isset($options[$key] ) ? esc_attr( $options[$key] ): '';
+    echo "<input type ='text' name=$name value='$value' size=6>";
+}
+
+/**
+* function to creat select section*
+*@since 0.1.0
+*@var function
+*@return void
+*@param $key -- string, $key in options
+*       $elements -- array, a set of values in options
+**/
+function fancy_slider_select_field($key,$elements){
+ 
+    $options = get_option( 'fancy_slider_options' );
+    $name = "fancy_slider_options[$key]";
+    /*the $key is a array*/
+    if ($key === 'slider_ctr') {
+        $options= &$options['slider_ctr'];
+        $key = 'mode';
+        $name = "fancy_slider_options[slider_ctr][mode]";
+    }  
+    $value  = empty( $options[$key] ) ? '': esc_attr( $options[$key] );
+    echo "<select name=$name>";
             foreach ( $elements as $element ) { 
-                $selected = selected( $mode_type , $element,false );
-                if ($name[1]){
-                    $suffix = $element === 1 ? '' : 'S';
-                }else{
-                    $suffix = '';
-                }               
-                echo "<option value='$element' $selected>" . __( strtoupper( $element ) . $name[0] . $suffix,'fancy-slider') . "</option>";
+                $selected = selected( $value , $element,false );              
+                echo "<option value='$element' $selected>" . __( strtoupper( $element ) ,'fancy-slider') . "</option>";
          } 
     echo '</select>';
 }
 
 /**
-* function to creat mode section callback *
+* function to creat center callback *
 *@since 0.1.0
 *@var function
 *@return void
+*@param $args -- array in callback function registered in  add_settings _field
 **/
-function fancy_slider_field_callback_mode(){
-    $name = array(' MODE',false);
-    $elements = array('responsive','sync','center','lazy loading');
-    fancy_slider_select_field_callback('mode_type',$elements,$name );
+function fancy_slider_field_callback_center($args){
+    $id = isset($args['id'])? $args['id'] : '';
+    $elements = array('true','false');
+    fancy_slider_select_field($id,$elements);
+    fancy_slider_text_field($id);  
 }
 
 /**
 * function to creat slider quantity section callback *
 *@since 0.1.0
 *@var function
+*@param $args -- array in callback function registered in  add_settings _field
 *@return void
 **/
-function fancy_slider_field_callback_sliders_qty(){ 
-    $name = array(' ITEM',true);
+function fancy_slider_field_callback_sliders_qty($args){ 
+    $id = isset($args['id'])? $args['id'] : '';
     $elements = array( 1,2,3,4,5,6);
-    fancy_slider_select_field_callback('slider_qty',$elements,$name);  
+    fancy_slider_select_field($id,$elements);
 }
 
 /**
 * function to creat scroll quantity section callback *
 *@since 0.1.0
 *@var function
+*@param $args -- array in callback function registered in  add_settings _field
 *@return void
 **/
-function fancy_slider_field_callback_scroll_qty(){
-    $name = array(' SCROLL',true);
+function fancy_slider_field_callback_scroll_qty($args){
+    $id = isset($args['id'])? $args['id'] : '';
     $elements = array(1,2,3,4);
-    fancy_slider_select_field_callback('scroll_qty',$elements,$name); 
+    fancy_slider_select_field($id,$elements); 
 }
 
 /**
 * function to creat autoplay section callback *
 *@since 0.1.0
 *@var function
+*@param $args -- array in callback function registered in  add_settings _field
 *@return void
 **/
-function fancy_slider_field_callback_autoplay(){
-    $name = array('',false);
+function fancy_slider_field_callback_autoplay($args){
+    $id = isset($args['id'])? $args['id'] : '';
     $elements =array('true','false');
-    fancy_slider_select_field_callback('slider_ap',$elements,$name);   
+    fancy_slider_select_field($id,$elements);   
 }
 
 /**
 * function to creat fade section callback *
 *@since 0.1.0
 *@var function
+*@param $args -- array in callback function registered in  add_settings _field
 *@return void
 **/
-function fancy_slider_field_callback_fade(){
-    $name = array('',false);
+function fancy_slider_field_callback_fade($args){
+    $id = isset($args['id'])? $args['id'] : '';
     $elements =array('true','false');
-    fancy_slider_select_field_callback('slider_fd',$elements,$name);   
+    fancy_slider_select_field($id,$elements);   
 }
 
 /**
 * function to creat dot indicator section callback *
 *@since 0.1.0
 *@var function
+*@param $args -- array in callback function registered in  add_settings _field
 *@return void
 **/
-function fancy_slider_field_callback_dot(){
-    $name = array('',false);
+function fancy_slider_field_callback_dot($args){
+    $id = isset($args['id'])? $args['id'] : '';
     $elements =array('true','false');
-    fancy_slider_select_field_callback('slider_dot',$elements,$name);   
+    fancy_slider_select_field($id,$elements);    
 }
 
 /**
 * function to creat infinite loop section callback *
 *@since 0.1.0
 *@var function
+*@param $args -- array in callback function registered in  add_settings _field
 *@return void
 **/
-function fancy_slider_field_callback_inf(){
-    $name = array('',false);
+function fancy_slider_field_callback_inf($args){
+    $id = isset($args['id'])? $args['id'] : '';
     $elements =array('true','false');
-    fancy_slider_select_field_callback('slider_inf',$elements,$name);   
+    fancy_slider_select_field($id,$elements);    
 }
 
 /**
 * function to creat transition speed text callback *
 *@since 0.1.0
 *@var function
+*@param $args -- array in callback function registered in  add_settings _field
 *@return void
 **/
-function fancy_slider_field_callback_trans_spd(){
-    $options = get_option( 'fancy_slider_options' );
-    $trans_spd = esc_attr( $options['trans_spd'] );
-    echo "<input type ='text' name='fancy_slider_options[trans_spd]' value='$trans_spd'>";
+function fancy_slider_field_callback_trans_spd($args){
+    $id = isset($args['id'])? $args['id'] : '';
+    fancy_slider_text_field($id);
 }
 
 /**
 * function to creat autoplay text callback *
 *@since 0.1.0
 *@var function
+*@param $args -- array in callback function registered in  add_settings _field
 *@return void
 **/
-function fancy_slider_field_callback_ap_spd(){
-    $options = get_option( 'fancy_slider_options' );
-    $ap_spd = esc_attr( $options['ap_spd'] );
-    echo "<input type ='text' name='fancy_slider_options[ap_spd]' value='$ap_spd'>";
+function fancy_slider_field_callback_ap_spd($args){
+    $id = isset($args['id'])? $args['id'] : '';
+    fancy_slider_text_field($id);
 }
 
-
+function sanitize_digital($d_value){  
+        if ( isset($d_value) ){
+            return preg_match("/^\d+\d+$|^\d$/", $d_value) ? true : false;    
+        }else{
+            return false;
+        }    
+}
 /**
 * function to sanitize fancy_slider_options before inputing into database *
 *@since 0.1.0
 *@var function
 *@return void
 **/
-function sanitize_options($input){
+function sanitize_options($input){  
     $new        = array();
-    $mode_type  = array( 'responsive','sync','center','lazy loading' );
-    $slider_qty = array( 1,2,3,4,5,6);
-    $scroll_qty = array( 1,2,3,4);
-    $slider_ap  = array( 'true','false' );
-    $slider_fd  = array( 'true','false' );
-    $slider_dot = array( 'true','false' );  
-    $slider_inf = array( 'true','false' );   
-    $d_keys     = array( 'ap_spd','trans_spd' );
-    $default    = 3000;
-    /* key as fancy_slider_options[key] , value as a set of values, which is whitelist related to the key */
-    $options    = array('mode_type'  => $mode_type ,
-                        'slider_qty' => $slider_qty ,
-                        'scroll_qty' => $scroll_qty,
-                        'slider_ap'  => $slider_ap,
-                        'slider_fd'  => $slider_fd,
-                        'slider_dot' => $slider_dot,
-                        'slider_inf' => $slider_inf
+    /* options whitelist */ 
+    $options    = array('slider_ctr' => array( 'mode' => array('true','false'),'pad' => 50),
+                        'slider_qty' => array( 1,2,3,4,5,6) ,
+                        'scroll_qty' => array( 1,2,3,4),
+                        'slider_ap'  => array( 'true','false' ),
+                        'slider_fd'  => array( 'true','false' ),
+                        'slider_dot' => array( 'true','false' ),
+                        'slider_inf' => array( 'true','false' ),
+                        'ap_spd'     => 3000,
+                        'trans_spd'  => 300
                     );
-    /* sanitize string selection : whitelist and wp sanitize function */
-    foreach ($options  as $key => $value ) {
-        if ( isset($input[$key]) ){
-           if ( in_array( $input[$key], $value) ){
-               $new[$key] = sanitize_text_field( $input[$key] ); 
-           }else{
-               $new[$key] = null;
-           }
-        }else{
-            $new[$key] = null;
-        }
-    }
 
-    /* sanitize digital input text */
-    foreach ($d_keys as $d_key) {
-        if ( isset($input[$d_key]) ){
-            if( preg_match("/^\d+\d+$|^\d$/", $input[$d_key]) ){
-                $new[$d_key] = (int)$input[$d_key];
-            }else {
-                $new[$d_key] = $default;
+    foreach ($options  as $option => $array ) {
+        $value = isset( $input[$option] ) ? $input[$option] : null;
+        /* array of option[key] */
+        if ($option === 'slider_ctr' ){       
+            /* select field whitelist sanitized */
+            if ( in_array($input['slider_ctr']['mode'], $array['mode']) ){
+                $new['slider_ctr']['mode'] = $input['slider_ctr']['mode']; 
+            }else{
+                $new['slider_ctr']['mode'] = null;
+            }
+            /* digital text field sanitized */
+            if ( sanitize_digital( $input['slider_ctr']['pad'] ) ){
+                $new['slider_ctr']['pad'] = (int)$input['slider_ctr']['pad']; 
+            }else{
+                $new['slider_ctr']['pad'] = $array['pad'] ;
             }
         }else{
-            $input[$d_key] = null;
+            if( is_array($array) ){
+                /* select field whitelist sanitized */
+                $new[$option] = in_array( $value, $array ) ? $value : null;
+            }else{
+                /* digital text field sanitized */
+                $new[$option] = sanitize_digital( $value ) ? (int)$value : (int)$array;
+            }               
         }
+        
     }
-
     return $new;
 }
