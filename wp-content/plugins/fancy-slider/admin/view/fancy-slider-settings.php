1aaebece68962ce81a665900187be43e3760ec6c
Hai <wordpree.dev@gmail.com>
2018-05-18 12:39:51 +1000

    created function fs_settings_field to set up field settings generally

--- wp-content/plugins/fancy-slider/admin/view/fancy-slider-settings.php
+++ wp-content/plugins/fancy-slider/admin/view/fancy-slider-settings.php
@@ -34,21 +34,12 @@ function fs_option_page_callback(){ ?>
 *@var function
 *@return void
 **/
-function fs_section_callback_basic(){
-	$text = 'In this section,fancy Slider can be setting up with different mode ,which a specific action would be applied';
-    esc_html_e( $text, 'fancy-slider' ); 
+function fs_section_callback_basic($args){
+    $settings = fs_settings_field();
+    $html = '<p>' . $settings[ $args['id'] ]['brief'] . '</p>';
+    _e( $html, 'fancy-slider' ); 
 }
 
-/**
-* function to creat advanced section callback *
-*@since 0.1.0
-*@var function
-*@return void
-**/
-function fs_section_callback_advanced(){
-    $text = 'In this section,fancy Slider can be setting up with different selective components,with all of which a specific action would be applied';
-    esc_html_e( $text, 'fancy-slider' ); 
-}
 
 /**
 * function to creat default fancy_slider_options if achieve failed*
@@ -58,65 +49,111 @@ function fs_section_callback_advanced(){
 **/
 function fancy_slider_default_opts(){
     return array(
-            'fs_std_no'=> array(
+            'fs_std_number'=> array(
                 'sli_qty' => '1',
                 'scr_qty' => '1',
                 'ap_spd'  => '3000',
-                'trs_spd' => '300'
+                'trs_spd' => '300',
+                'pad'     => '50',
             ),
-            'fs_std_slt'=>array(
-                 'ap'  => 'false',
-                 'fd'  => 'false',
-                 'dot' => 'false',
-                 'inf' => 'false'
-            )                                         
+            'fs_std_checkbox'=> 'arrow',
+            'fs_md_radio' => 'slide'                                       
     );
 }
+function fs_sanitize_options($input){
+    return $input;
+}
+function fs_fields_callback($args){
 
+}
 /**
-* function to creat text field display*
+* function to generate field settings *
 *@since 0.1.0
 *@var function
-*@return void
-*@param $id (string),  key of array fancy_slider_options
-*       $key (string), key of array fancy_slider_options[$id] if it is a array
-*       $label (string), label
+*@return settings array
 **/
-function fs_text_field($id,$key,$label){
-
-    $options = get_option( 'fancy_slider_options' ,fancy_slider_default_opts());
-    $name = "fancy_slider_options[$id]";
-    if ( is_array($options[$id]) ){
-        $options= &$options[$id];
-        $name = "fancy_slider_options[$id][$key]";
-        $id = $key;
-    }
-    $value = isset($options[$id] ) ?  $options[$id] : '';
-    echo "<label for='$name'>$label</label>";
-    echo "<input type ='text' id= '$name' name= '$name' value= '$value' size=6>";
+function fs_settings_field(){
+    $settings['basic'] = array(
+        'title'  => 'Slider Basic Settings',
+        'brief'  => 'Slider basic settings are listed below',
+        'page'   => 'fancy-slider',
+        'scb'    => 'fs_sanitize_options',
+        'fcb'    => 'fs_fields_callback',
+        'opt'    => 'fancy_slider_options',
+        'fields' => array(
+            array(
+                'id' => 'multi_checkbox_format',
+                'sub_title' => 'Format Setting',
+                'brief'     =>  'Please tick your slider format',
+                'type'      => 'checkbox',
+                'options'   => array(
+                    'autoplay'  => 'Autoplay',
+                    'dot'       => 'Dot Indicator',
+                    'infinite'  => 'Infinite Loop',
+                    'centre'    => 'Centre Mode',
+                    'arrow'     => 'Next/Prev Arrows'
+                )
+            ),
+            array(
+                'id' => 'radio_mode',
+                'sub_title' => 'Mode Selection',
+                'brief'     =>  'Here,slider mode can be selected',
+                'type'      => 'radio',
+                'options'   => array(
+                    'slide'  => 'Slide',
+                    'fade'       => 'Fade'
+                )
+            ),
+            array(
+                'id' => 'number_digital',
+                'sub_title' => 'Digital Parameter',
+                'brief'     =>  'Please input your parameter here',
+                'type'      =>  'number'
+            )                 
+        )
+    );
+    $settings = apply_filters( 'fancy_slider_settings', $settings );
+    return $settings;
 }
 
-
 /**
-* function to creat number field display*
+* function to display $type input field display *
 *@since 0.1.0
 *@var function
 *@return void
 *@param $id (string),  key of array fancy_slider_options
 *       $key (string), key of array fancy_slider_options[$id] if it is a array
 *       $label (string), label
+*       $type (string), input type
 **/
-function fs_number_field($id,$key,$label){
+function fs_text_field($id,$label){
     $options = get_option( 'fancy_slider_options',fancy_slider_default_opts() );
     $name = "fancy_slider_options[$id]";
-    if ( is_array($options[$id]) ){
-        $options= &$options[$id];
-        $name = "fancy_slider_options[$id][$key]";
-        $id = $key;
+    $value = $options[$id];
+    switch ($id) {
+        case 'fs_std_checkbox':
+            // $options= &$options[$id];
+            // foreach ($label as $key => $value) {
+            //     $name = "fancy_slider_options[$id][$key]";
+            // }
+            foreach ($label as $key => $title) {
+                $checked = checked($value,$key,false);
+                $name = "fancy_slider_options[]";
+                echo "<li>" . "<label>" .
+                    "<input type ='checkbox' name='$name' value='$key' $checked >" . "$title" .
+                 "</label></li>";
+            }
+            break;
+        
+        case 'fs_md_radio':
+            foreach ($label as $key => $title) {
+                $checked = checked($value,$key,false);
+                echo "<li>" . "<label>" .
+                    "<input type ='radio' name='$name' value='$key' $checked >" . "$title" .
+                 "</label></li>";
+            }
+            break;
     }
-    $value = isset($options[$id] ) ?  $options[$id] : '';
-    echo "<label for='$name'>$label</label>";
-    echo "<input type ='number' id= '$name' name= '$name' value= '$value' >";
 }
 
 /**
@@ -151,7 +188,7 @@ function fs_select_field($id,$key,$label){
 
 
 /**
-* text field callback function *
+* number field callback function *
 *@since 0.1.0
 *@var function
 *@return void
@@ -159,30 +196,10 @@ function fs_select_field($id,$key,$label){
 **/
 function fs_field_callback_text($args){
     $id = isset($args['id'])? $args['id'] : '';
-    $labels = empty($args['label_for'])?  '' : $args['label_for'];
-    foreach ($labels as $key => $value) {
-        fs_text_field($id,$key,$value );
-    }
-
-}
-
-/**
-* number field callback function *
-*@since 0.1.0
-*@var function
-*@return void
-*@param $args (array), args in add_settings _field 
-**/
-function fs_field_callback_number($args){
-    $id = isset($args['id'])? $args['id'] : '';
-    $labels = empty($args['label'])?  '' : $args['label'];
-    echo '<fieldset>';
-    foreach ($labels as $key => $value) {
-        echo '<ul>';
-        fs_number_field($id,$key,$value );
-        echo '</ul>';
-    }
-    echo '</fieldset>';
+    $label = empty($args['label'])?  '' : $args['label'];
+    echo '<fieldset>' . '<ul>';
+    fs_text_field($id,$label);
+    echo '</ul>' . '</fieldset>';
 }
 
 /**
@@ -244,28 +261,34 @@ function fs_sanitize_bool($b_value){
 *@var function
 *@return void
 **/
-function fs_sanitize_options($input){  
-    $new        = array();
-    /* options whitelist */ 
-    $options    = array(
-        'fs_std_slt' => array( 'ap','fd','dot','inf' ),
-        'fs_std_no' => array( 'sli_qty','scr_qty','ap_spd','trs_spd'),
-    );
-    foreach ($options  as $option => $array ) {
-        if ( ! empty( $array ) ){
-            foreach ($array as $value ) {
-                $temp_value = isset( $input[$option][$value]) ? $input[$option][$value] : null;
-                switch ( $option ) {
-                    case 'fs_std_slt':
-                        $new[$option][$value] = fs_sanitize_bool($temp_value) ? $temp_value : fancy_slider_default_opts()[$option][$value];
-                        break;
-                    
-                    case 'fs_std_no':
-                        $new[$option][$value] =  fs_sanitize_digital($temp_value) ? $temp_value : fancy_slider_default_opts()[$option][$value];
-                        break;
-                }    
-            }  
-        }           
-    }
-    return $new;
+function sanitize_options($input){  
+    // $new        = array();
+    // $options_array    = array(
+        
+    //     'fs_std_number'  => array( 'sli_qty','scr_qty','ap_spd','trs_spd','pad')
+    // );
+    // $options_single = array(
+    //     'fs_md_radio' => array('slide','fade'),
+    //     'fs_std_checkbox'=> array( 'autoplay','dot','infinite','centre' ,'arrow')
+    // );
+    // foreach ($options_array  as $option => $array ) {
+    //     foreach ($array as $value ) {
+    //         $temp_value = isset( $input[$option][$value]) ? $input[$option][$value] : null;
+    //         switch ( $option ) {
+    //             case 'fs_std_checkbox':
+    //                 $new[$option][$value] = $temp_value ;
+    //                 break;
+                
+    //             case 'fs_std_number':
+    //                 $new[$option][$value] = fs_sanitize_digital($temp_value) ? $temp_value : fancy_slider_default_opts()[$option][$value];
+    //                 break;
+    //         }    
+    //     }       
+    // }
+
+    // foreach ($options_single as $key => $value) {
+    //     $new[$key] = isset( $input[$key] ) ? $input[$key]: null;
+    //     $new[$key] = in_array($new[$key], $value) ? $new[$key] : fancy_slider_default_opts()[$key];
+    // }
+    return $input;
 }
