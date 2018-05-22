279f0bb93febedc0cc4f66fabc1020a45ac70b4b
Hai <wordpree.dev@gmail.com>
2018-05-22 17:12:39 +1000

    dynamic display fields when ticking different options

--- wp-content/plugins/fancy-slider/sources/js/fancy-slider-admin.js
+++ wp-content/plugins/fancy-slider/sources/js/fancy-slider-admin.js
@@ -1,7 +1,38 @@
 (function( $ ){
   'use strict';
-   $( document ).ready(function(){
+
+   function checkboxDisplay(_class,_id){
+	   var $target = $('ul.'+ _class + ' li:last');
+   	   var prop = $('ul.'+ _class + ' ' +'#'+_id).prop('checked');
+   	   if (prop === true){
+		   $target.show();
+	   }else{
+		   	$target.hide();
+	   }
+	   $('ul.'+ _class + ' li:nth-child(1)').find('input').click(function(){
+		    $target.toggle();
+	   });
+   }
    
+   function radioDisplay(_class,_id){
+	   var prop = $('ul.'+ _class + ' ' +'#'+_id).prop('checked');
+	   var $target = $('ul.'+ _class + ' li').slice(2);
+	   if (prop === true){
+		   $target.hide();
+	   }
+	   $('ul.'+ _class + ' li:nth-child(1)').find('input').click(function(){
+		    $target.hide();
+	   });
+	   $('ul.'+ _class + ' li:nth-child(2)').find('input').click(function(){
+		    $target.show();
+	   });
+   }
+
+   $( document ).ready(function(){
+	   radioDisplay('lazyload','wpfs_lazyload_progressive');
+	   checkboxDisplay('centre','wpfs_centre_centre');
+	   checkboxDisplay('autoplay','wpfs_autoplay_autoplay');
+	   radioDisplay('animation','wpfs_animation_slide');
    });
 
    $( window ).load(function(){
