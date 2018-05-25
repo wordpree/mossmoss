1ef0834e83e6f8dbcb2cdb6247a23749279ca276
Hai <wordpree.dev@gmail.com>
2018-05-25 14:42:21 +1000

    added settings page number input check and created related popup style

--- wp-content/plugins/fancy-slider/sources/js/fancy-slider-admin.js
+++ wp-content/plugins/fancy-slider/sources/js/fancy-slider-admin.js
@@ -1,6 +1,7 @@
 (function( $ ){
   'use strict';
    
+   /* hide or show sub input area when a specific item is selected */
    function radioDisplay(_class,_id){
 	   var prop = $('ul.'+ _class + ' ' +'#'+_id).prop('checked');
 	   var $target = $('ul.'+ _class + ' li').slice(2);
@@ -14,12 +15,39 @@
 		    $target.hide();
 	   });
    }
+   /* check a number input area valid or not */
+   function numberChk(){
 
+		var $inputs = $('input[type="number"]');
+		var pat = /^\d+$/;
+		$inputs.each(function(index) {
+			$(this).parent('label').before('<span class="invalid">invalid figure</span>');	 
+		});
+
+	   $('.wrap form').on('submit',function(e){
+		   	$inputs.each(function(index) {
+		   		var $this = $(this);
+		   		var target = $this.val();
+		   		if (! pat.test( target ) ){
+		   			$this.focus().parent().prev().addClass('activate');
+				    e.preventDefault();
+				    return false;
+			   	}
+		   	});		   	
+	   });
+
+   	$('.wrap form').on('click',function(){
+	    $('span.activate ').blur().removeClass('activate');
+	});
+
+
+   }
    $( document ).ready(function(){
 	   radioDisplay('lazyload','wpfs_lazyload_progressive');
 	   radioDisplay('centre','wpfs_centre_disable');
 	   radioDisplay('autoplay','wpfs_autoplay_disable');
 	   radioDisplay('animation','wpfs_animation_slide');
+	   numberChk();   
    });
 
    $( window ).load(function(){
