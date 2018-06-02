2a43eecfcc6b5484ff1dcb48db9cbbf6f210eb31
Hai <wordpree.dev@gmail.com>
2018-06-02 11:19:23 +1000

    changed require path to absolute server path

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -2,9 +2,9 @@
 /*the functions.php of a child theme does not override its counterpart from the parent. 
 Instead, it is loaded in addition to the parent’s functions.php. 
 (Specifically, it is loaded right before the parent’s file.) */
-require 'inc/storefront-child-functions.php';
-require 'inc/storefront-child-template-hooks.php';
-require 'inc/storefront-child-template-functions.php';
-require 'inc/class-storefront-child-customizer.php';
-require 'inc/class-storefront-child.php'; 
+require get_stylesheet_directory() . '/inc/storefront-child-functions.php';
+require get_stylesheet_directory() . '/inc/storefront-child-template-hooks.php';
+require get_stylesheet_directory() . '/inc/storefront-child-template-functions.php';
+require get_stylesheet_directory() . '/inc/class-storefront-child-customizer.php';
+require get_stylesheet_directory() . '/inc/class-storefront-child.php'; 
 //require 'inc/woocommerce/class-storefront-woocommerce.php';
